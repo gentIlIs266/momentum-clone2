9c9b31760faa643e233a3cebe1c8c87a2dd79a38
nigreos625 <160678470+gentIlIs266@users.noreply.github.com>
2024-04-07 23:49:11 +0900

    log in, greeting message, reset css

---
+++ assets/scripts/login.js
@@ -0,0 +1,22 @@
+const logInForm = document.querySelector("#log-in__form");
+const greetingMessage = document.querySelector("#greeting-message");
+const savedUsername = localStorage.getItem("username");
+function logInSubmitObserver (event) {
+    event.preventDefault();
+    const logInInput = document.querySelector("#log-in__input");
+    const typedUsername = logInInput.value;
+    localStorage.setItem("username", typedUsername);
+    logInForm.classList.add("is-hidden");
+    showGreetingMessage(typedUsername);
+}
+function showGreetingMessage (typedUsername) {
+    greetingMessage.textContent = `Hello, ${typedUsername}`
+    greetingMessage.classList.remove("is-hidden");
+}
+if (savedUsername === null) {
+    logInForm.classList.remove("is-hidden");
+    logInForm.addEventListener("submit", logInSubmitObserver);
+} else {
+    greetingMessage.classList.remove("is-hidden");
+    showGreetingMessage(savedUsername);
+}
