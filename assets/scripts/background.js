2d58d2c03030f9c419c54fae8d7c2ead36d1a383
nigreos625 <160678470+gentIlIs266@users.noreply.github.com>
2024-04-08 01:01:14 +0900

    images, background.js

---
+++ assets/scripts/background.js
@@ -0,0 +1,16 @@
+const backgroundImages = [
+        "0.jpg",
+        "1.jpg",
+        "2.jpg",
+        "3.jpg",
+        "4.jpg",
+        "5.jpg",
+        "6.jpg",
+        "7.jpg",
+        "8.jpg",
+        "9.jpg"
+]
+const chooseBg = backgroundImages[Math.round(Math.random() * backgroundImages.length)];
+const doc_bg = document.createElement("img");
+doc_bg.src = `images/${chooseBg}` 
+document.body.appendChild(doc_bg);
